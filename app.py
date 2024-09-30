adbbeed06aaaf393117da6bf007dd95514d50db3
Pav Dhillon <93717054+pavdhillon@users.noreply.github.com>
2024-09-30 15:05:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
 from flask_cors import CORS  # Import CORS
+import pandas
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
